3d305727f4f1adb43fc614c5aadcb7a5e833165e
conor heffernan <conorheffernan@hubspot.com>
2020-12-09 15:39:44 +0000

    interactively generate the csv file

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ from score_writer.game import Game
 from score_writer.game_writer import GameWriter
 from Team.TeamStats import TeamStats
 
+def parseInput(input):
+    input = input.lower()[0]
+    if(input == "y"):
+        return True
+    return False
 
 
 FILE_PATH = "data/2019_games.csv"
+
+userInput = input("Do you want to generate CSV? (y/n)\n> ")
+shouldGenCsv = parseInput(userInput)
+
 
 # ---------- (UN)COMMENT THIS TO MAKE CSV FILE / APPEND TO EXISTING CSV ----------------
 rankings_frame = pd.read_csv("data/ranking.csv")
@@ -20,23 +29,25 @@ teams = pd.read_csv("data/teams.csv")['TEAM_ID']
 games_list = []
 team_stats = TeamStats(teams)
 
-# for index, game_date in enumerate(games_frame['GAME_DATE_EST']):
-#     year = int(game_date.split("-")[0])
-#     if year == 2019:
-#         home_team_id = games_frame['HOME_TEAM_ID'].iloc[index];
-#         away_team_id = games_frame['VISITOR_TEAM_ID'].iloc[index];
-#         home_team = team_stats.getTeam(home_team_id)
-#         away_team = team_stats.getTeam(away_team_id)
-#         home_team_win = games_frame['HOME_TEAM_WINS'].iloc[index]
-#         team_stats.recordGame([home_team_id, away_team_id, home_team_win])
-#         currentGame = Game(home_team, away_team, game_date, rankings_frame,home_team_win)
-#
-#         if (currentGame.hasSufficientData()):
-#             games_list.append(currentGame)
-#
-# # print(games_list[0]) this shows how we can print games to examine the feature values
-# game_writer = GameWriter(FILE_PATH, games_list)
-# game_writer.write()
+if(shouldGenCsv):
+    print("Generating data. This will take a minute....")
+    for index, game_date in enumerate(games_frame['GAME_DATE_EST']):
+        year = int(game_date.split("-")[0])
+        if year == 2019:
+            home_team_id = games_frame['HOME_TEAM_ID'].iloc[index];
+            away_team_id = games_frame['VISITOR_TEAM_ID'].iloc[index];
+            home_team = team_stats.getTeam(home_team_id)
+            away_team = team_stats.getTeam(away_team_id)
+            home_team_win = games_frame['HOME_TEAM_WINS'].iloc[index]
+            team_stats.recordGame([home_team_id, away_team_id, home_team_win])
+            currentGame = Game(home_team, away_team, game_date, rankings_frame,home_team_win)
+
+            if (currentGame.hasSufficientData()):
+                games_list.append(currentGame)
+
+    # print(games_list[0]) this shows how we can print games to examine the feature values
+    game_writer = GameWriter(FILE_PATH, games_list)
+    game_writer.write()
 # ------------------------------------------------------------------------------------
 
 csv_dataframe = pd.read_csv(FILE_PATH)
