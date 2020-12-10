6b13b483fb3684ee93bb559c5fea121a4a129bf9
conor heffernan <conorheffernan@hubspot.com>
2020-12-10 22:05:24 +0000

    updated rankings scraper so that it uses the team_id

--- score_writer/rankings_scraper.py
+++ score_writer/rankings_scraper.py
@@ -3,6 +3,10 @@ from lxml import html
 import csv
 import os
 from pathlib import Path
+import json
+
+with open("../Team/team_config.json") as team_config:
+    team_name_to_id_dict = json.load(team_config)
 
 PUPPETEER_URL_PREFIX = "http://localhost:3000?url="
 URL_TEMPLATE = PUPPETEER_URL_PREFIX + "https://www.basketball-reference.com/leagues/NBA_{year}_standings.html#expanded_standings::none"
@@ -11,6 +15,16 @@ URL_TEMPLATE = PUPPETEER_URL_PREFIX + "https://www.basketball-reference.com/leag
 def getCellValueInRow(row,data_stat):
     return row.xpath(f'td[@data-stat="{data_stat}"]')[0].text
 
+def getTeamId(teamName):
+    nameArray = teamName.split(" ")
+
+    if(nameArray[0] == "Los"):
+        franchise =  f"LA {nameArray[2]}"
+    elif(len(nameArray) > 2 and nameArray[0] != "Portland"):
+        franchise = f"{nameArray[0]} {nameArray[1]}"
+    else:
+        franchise = nameArray[0]
+    return team_name_to_id_dict[franchise]
 
 
 start_year = int(input("Choose a year to get the standings for\n > "))
@@ -22,10 +36,9 @@ if(is_file_existing):
     os.remove(outputFile)
 
 with open(outputFile, 'a') as output_csv:
-    fieldnames = ['TEAM_NAME', 'OVERALL', 'HOME_RECORD', 'ROAD_RECORD', 'POST_SEASON_RECORD', 'RECORD_CLOSE_GAMES', "RECORD_NORMAL_GAMES"]
+    fieldnames = ['TEAM_ID','TEAM_NAME', 'OVERALL', 'HOME_RECORD', 'ROAD_RECORD', 'POST_SEASON_RECORD', 'RECORD_CLOSE_GAMES', "RECORD_NORMAL_GAMES"]
     writer = csv.DictWriter(output_csv, fieldnames=fieldnames, lineterminator='\n')
-    if not is_file_existing:
-        writer.writeheader()
+    writer.writeheader()
 
     current_date_url = URL_TEMPLATE.format(year=start_year)
     response_data = requests.get(current_date_url)
@@ -38,13 +51,14 @@ with open(outputFile, 'a') as output_csv:
         row_has_data = len(rankCell) > 0
         if(row_has_data and rankCell[0].text != "Rk"):
             team_name = row.xpath(f'td[@data-stat="team_name"]/a')[0].text
+            team_id = getTeamId(team_name)
             overall = getCellValueInRow(row,"Overall")
             home_record = getCellValueInRow(row, "Home")
             road_record = getCellValueInRow(row,"Road")
             post_season_record = getCellValueInRow(row,"Post")
             record_close_games = getCellValueInRow(row,"3")
             record_normal_games = getCellValueInRow(row, "10")
-            writer.writerow({'TEAM_NAME': team_name, 'OVERALL': overall, 'HOME_RECORD': home_record, 'ROAD_RECORD': road_record, "POST_SEASON_RECORD":post_season_record, "RECORD_CLOSE_GAMES":record_close_games, "RECORD_NORMAL_GAMES": record_normal_games})
+            writer.writerow({'TEAM_ID': team_id,'TEAM_NAME': team_name, 'OVERALL': overall, 'HOME_RECORD': home_record, 'ROAD_RECORD': road_record, "POST_SEASON_RECORD":post_season_record, "RECORD_CLOSE_GAMES":record_close_games, "RECORD_NORMAL_GAMES": record_normal_games})
 
 
 
