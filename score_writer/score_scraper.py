8e9e21509d4d7433af02584c4dbfe4f13d9e7403
conor heffernan <conorheffernan@hubspot.com>
2020-12-12 02:20:25 +0000

    removed unused function

--- score_writer/score_scraper.py
+++ score_writer/score_scraper.py
@@ -32,18 +32,15 @@ class ScoreScraper:
                 game_results_dict = {'date':str(date)}
                 game_results_dict = self.get_teams_and_scores_dict(game_result_element,game_results_dict)
                 game_result = self.get_game_result(game_result_element)
-                '''_______________________'''
+
                 home_team = team_stats.getTeam(game_results_dict["home_team_id"])
                 away_team = team_stats.getTeam(game_results_dict["away_team_id"])
                 self.get_home_and_road_record(game_results_dict, home_team, away_team)
 
-
-                '''_______________________'''
-
                 self.get_win_loss_stats(game_results_dict, home_team, away_team)
                 game_results_dict["is_home_winner"] = game_result
-                # we record the games as we go so that we can manually compute features about the teams before the game such as
-                # home record, road record etc
+
+                # record the game to compute stats on the fly
                 team_stats.recordGame( [game_results_dict["home_team_id"], game_results_dict["away_team_id"], game_results_dict["is_home_winner"],game_results_dict['home_team_score'] , game_results_dict['away_team_score'] ])
                 self.results_list.append(game_results_dict)
 
@@ -75,29 +72,6 @@ class ScoreScraper:
         game_results_dict["away_team_road_wins"] = away_team_record["AWAY_WINS"]
         game_results_dict["away_team_road_loses"] = away_team_record["AWAY_LOSES"]
 
-    def getConferenceStandings(self,html_tree):
-        conferenceStandings = {}
-        for conference in ["E", "W"]:
-            games_played_today = len(html_tree.xpath(f"//table[@id='confs_standings_{conference}']")) > 0
-            if(games_played_today):
-                conf_standing = html_tree.xpath(f"//table[@id='confs_standings_{conference}']")[0]
-                conf_rows = conf_standing.xpath(".//tr")
-
-                for row in conf_rows:
-                    team_name = row.xpath(".//th/a")
-                    team_record = {}
-                    if (len(team_name) > 0):
-                        team_name = team_name[0].text
-                        team_id = self.getTeamId(team_name)
-                        cellList = row.xpath(".//td")
-                        team_record["team_name"] = team_name
-                        team_record["wins"] =  int(cellList[0].text)
-                        team_record["loses"] = int(cellList[1].text)
-                        team_record["points_per_game"] = float(cellList[4].text)
-                        team_record["points_against_per_game"] = float(cellList[5].text)
-                        conferenceStandings[team_id] = team_record
-
-        return conferenceStandings
 
     def get_win_loss_stats(self, team_score_dict,home_team, away_team):
         team_score_dict["home_team_wins"] = home_team.getWins()
@@ -108,46 +82,7 @@ class ScoreScraper:
         team_score_dict["away_team_loses"] = away_team.getLoses()
         team_score_dict["away_team_points_per_game"] = away_team.getPointsPerGame()
         team_score_dict["away_team_points_against_per_game"] = away_team.getPointsConcededPerGame()
-    '''
-    The standings data scraped is updated with the result 
-    so we need to decrement the stats to adjust for this
-    '''
-    def add_team_record_to_dict(self, team_score_dict, conference_standings, game_result):
-        home_team_id = team_score_dict['home_team_id']
-        home_team_score = team_score_dict['home_team_score']
-        away_team_id = team_score_dict['away_team_id']
-        away_team_score = team_score_dict['away_team_score']
-        home_team_won = game_result
 
-        home_team_record = conference_standings[home_team_id]
-        away_team_record = conference_standings[away_team_id]
-
-        home_team_games_played  = home_team_record["wins"] + home_team_record["loses"]
-        away_team_games_played = away_team_record["wins"] + away_team_record["loses"]
-
-
-        if home_team_won == 1:
-            home_team_record["wins"] = home_team_record["wins"] - 1
-            away_team_record["loses"] = away_team_record["loses"] - 1
-        else:
-            home_team_record["loses"] = home_team_record["loses"] - 1
-            away_team_record["wins"] = away_team_record["wins"] -1
-
-        home_team_record["points_per_game"] = 0 if home_team_games_played ==1 else  ((home_team_record["points_per_game"] * home_team_games_played) - home_team_score) / home_team_games_played - 1
-        home_team_record["points_against_per_game"] = 0 if home_team_games_played ==1 else  ((home_team_record["points_against_per_game"] * home_team_games_played) - away_team_score) / home_team_games_played - 1
-        away_team_record["points_per_game"] = 0 if away_team_games_played ==1 else ((away_team_record["points_per_game"] * away_team_games_played) - away_team_score) / away_team_games_played - 1
-        away_team_record["points_against_per_game"] = 0 if away_team_games_played == 1 else ((away_team_record["points_against_per_game"] * away_team_games_played) - home_team_score) / away_team_games_played - 1
-
-        team_score_dict["home_team_wins"] = home_team_record["wins"]
-        team_score_dict["home_team_loses"] = home_team_record["loses"]
-        team_score_dict["home_team_points_per_game"] = home_team_record["points_per_game"]
-        team_score_dict["home_team_points_against_per_game"] = home_team_record["points_against_per_game"]
-        team_score_dict["away_team_wins"] = away_team_record["wins"]
-        team_score_dict["away_team_loses"] = away_team_record["loses"]
-        team_score_dict["away_team_points_per_game"] = away_team_record["points_per_game"]
-        team_score_dict["away_team_points_against_per_game"] = away_team_record["points_against_per_game"]
-
-        return team_score_dict
 
     def get_teams_and_scores_dict(self, game_result_element,team_score_dict):
         winner_loser_order = game_result_element.xpath(".//tr[@class='winner' or @class='loser']")
