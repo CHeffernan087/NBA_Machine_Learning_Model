cb37292bc47c5339c6bdbc96e826e6ae8745ef3e
BurkeO <burkeowen98@gmail.com>
2020-12-12 17:21:33 +0000

    adding helper func for cross val with different models, hyper params

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import numpy as np
 import pandas as pd
-from CSVGenerator import CSVGenerator
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import RidgeClassifier
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVC
+
+
+from CSVGenerator import CSVGenerator
+from helper_functions import cross_validate, HyperParam
 
 
 def parseInput(user_input):
@@ -13,7 +22,7 @@ def parseInput(user_input):
     return False
 
 
-YEAR_TO_GENERATE = 2019
+YEAR_TO_GENERATE = 2018
 
 userInput = input("Do you want to generate CSV? (y/n)\n> ")
 shouldGenCsv = parseInput(userInput)
@@ -21,7 +30,7 @@ shouldGenCsv = parseInput(userInput)
 if shouldGenCsv:
     CSVGenerator(YEAR_TO_GENERATE).generate()
 
-YEAR_FOR_TESTING = 2020
+YEAR_FOR_TESTING = 2019
 FILE_PATH_TEST = f"data/{YEAR_FOR_TESTING}_games.csv"
 FILE_PATH_TRAIN = f"data/{YEAR_TO_GENERATE}_games.csv"
 
@@ -31,7 +40,24 @@ num_columns = len(training_csv_dataframe.columns)
 
 x_input_features = training_csv_dataframe.iloc[:, range(0, num_columns - 1)]
 y_output_data = training_csv_dataframe.iloc[:, [num_columns - 1]]
-model = LogisticRegression(penalty='none', max_iter=500).fit(x_input_features, np.array(y_output_data).ravel())
+model = LogisticRegression(penalty='none', max_iter=900).fit(x_input_features, np.array(y_output_data).ravel())
+
+# cross_validate(LogisticRegression, HyperParam.C, [0.001, 0.01, 0.1, 1, 10, 50, 80], x_input_features,
+#                y_output_data, max_iter=900)
+
+# any more than 2 and it will take a good while. Also need a decent amount of iterations to avoid warning
+# cross_validate(LogisticRegression, HyperParam.POWER, [1, 2], x_input_features,
+#                y_output_data, max_iter=5000)
+
+# cross_validate(SVC, HyperParam.GAMMA, [0.001, 0.01, 0.1, 1, 10, 50, 80], x_input_features,
+#                y_output_data, max_iter=900)
+#
+# cross_validate(SVC, HyperParam.C, [0.001, 0.01, 0.1, 1, 10, 50, 80], x_input_features,
+#                y_output_data, max_iter=900)
+#
+# cross_validate(RidgeClassifier, HyperParam.ALPHA, [0.001, 0.01, 0.1, 1, 10, 50, 80], x_input_features,
+#                y_output_data, max_iter=900)
+
 
 test_x_input_features = testing_csv_dataframe.iloc[:, range(0, num_columns - 1)]
 test_y_output_data = testing_csv_dataframe.iloc[:, [num_columns - 1]]
