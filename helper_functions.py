c38b6fe1e2aba319abd70a3618c5f8f8b533f216
BurkeO <burkeowen98@gmail.com>
2020-12-18 19:22:07 +0000

    added cross_val for 3 types of model & print accuracy. Logistic is best

--- helper_functions.py
+++ helper_functions.py
@@ -6,26 +6,29 @@ import numpy as np
 from matplotlib import pyplot
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import RidgeClassifier
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, log_loss
 from sklearn.model_selection import KFold
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
 
 class HyperParam(Enum):
     C = 1
     POWER = 2
     GAMMA = 3
-    ALPHA = 4
+    K = 4
 
 
-def cross_validate(model_type: Union[Type[LogisticRegression], Type[RidgeClassifier], Type[SVC]],
+def cross_validate(model_type: Union[Type[LogisticRegression], Type[KNeighborsClassifier], Type[SVC]],
                    hyper_param: HyperParam, hyper_param_values_list, x_training_data, y_training_data, k_fold_splits=10,
-                   penalty_type="l2", max_iter=None):
+                   penalty_type="l2", max_iter=None, weights=None):
     print("cross validating....")
     x_training_data = np.array(x_training_data)
     y_training_data = np.array(y_training_data)
-    mean_error = []
+    log_loss_list = []
     standard_dev = []
 
     for param in hyper_param_values_list:
@@ -36,14 +39,17 @@ def cross_validate(model_type: Union[Type[LogisticRegression], Type[RidgeClassif
             model_params['C'] = param
         if max_iter is not None:
             model_params['max_iter'] = max_iter
+        if weights is not None:
+            model_params["weights"] = weights
         if model_type == LogisticRegression:
             model_params['penalty'] = penalty_type
         elif model_type == SVC and hyper_param == HyperParam.GAMMA:
             model_params['gamma'] = param
-        elif model_type == RidgeClassifier and hyper_param == HyperParam.ALPHA:
-            model_params['alpha'] = param
+        elif model_type == KNeighborsClassifier:
+            model_params["n_neighbors"] = param
 
         model = model_type(**model_params)
+        pipeline = make_pipeline(StandardScaler(), model)
 
         if hyper_param == HyperParam.POWER:
             x_training_data = PolynomialFeatures(param).fit_transform(x_training_data)
@@ -51,15 +57,15 @@ def cross_validate(model_type: Union[Type[LogisticRegression], Type[RidgeClassif
         temp = []
         kf = KFold(n_splits=k_fold_splits)
         for train, test in kf.split(x_training_data):
-            model.fit(x_training_data[train], y_training_data[train].ravel())
-            y_pred = model.predict(x_training_data[test])
-            temp.append(mean_squared_error(y_training_data[test], y_pred))
-        mean_error.append(numpy.array(temp).mean())
+            pipeline.fit(x_training_data[train], y_training_data[train].ravel())
+            y_pred = pipeline.predict(x_training_data[test])
+            temp.append(log_loss(y_training_data[test], y_pred))
+        log_loss_list.append(numpy.array(temp).mean())
         standard_dev.append(numpy.array(temp).std())
 
-    pyplot.errorbar(hyper_param_values_list, mean_error, yerr=standard_dev, label="Standard Deviation")
+    pyplot.errorbar(hyper_param_values_list, log_loss_list, yerr=standard_dev, label="Standard Deviation")
     pyplot.title(f"Prediction error vs {hyper_param} - {model_type.__name__}")
     pyplot.xlabel(f"{hyper_param} Value")
-    pyplot.ylabel("Mean square error")
+    pyplot.ylabel("Log loss")
     pyplot.legend()
     pyplot.show()
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, log_loss
-from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score, plot_confusion_matrix
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-
+from helper_functions import cross_validate, HyperParam
 from CSVGenerator import CSVGenerator
+from sklearn.svm import SVC
+from matplotlib import pyplot
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def parseInput(user_input):
@@ -46,25 +48,6 @@ y_output_data = training_csv_dataframe.iloc[:, [num_columns - 1]]
 test_x_input_features = testing_csv_dataframe.iloc[:, range(0, num_columns - 1)]
 test_y_output_data = testing_csv_dataframe.iloc[:, [num_columns - 1]]
 
-log_loss_list = []
-standard_dev = []
-x_training_data = np.array(x_input_features)
-y_training_data = np.array(y_output_data)
-for c_value in [0.001, 0.01, 0.1, 1, 5, 10, 15, 20]:
-    logistic_model = LogisticRegression(class_weight='auto', max_iter=900, C=c_value)
-
-    pipeline = make_pipeline(StandardScaler(), logistic_model)
-    pipeline.fit(x_training_data, np.array(y_training_data).ravel())
-
-    temp = []
-    kf = KFold(n_splits=10)
-    for train, test in kf.split(x_training_data):
-        pipeline.fit(x_training_data[train], y_training_data[train].ravel())
-        y_pred = pipeline.predict(x_training_data[test])
-        temp.append(log_loss(y_training_data[test], y_pred))
-    log_loss_list.append(np.array(temp).mean())
-    standard_dev.append(np.array(temp).std())
-
 # num_features_to_accuracy_dit = {}
 # for n in range(len(x_input_features.columns)):
 #     selector = RFE(logistic_model, n_features_to_select=n + 1)
@@ -81,38 +64,47 @@ for c_value in [0.001, 0.01, 0.1, 1, 5, 10, 15, 20]:
 #     if value >= current_accuracy:
 #         ideal_num_features = key
 
-# cross_validate(LogisticRegression, HyperParam.C, [0.001, 0.01, 0.1, 1, 10, 50, 80], x_input_features,
-#                y_output_data, max_iter=900)
-#
-# # any more than 2 and it will take a good while. Also need a decent amount of iterations to avoid warning
-# cross_validate(LogisticRegression, HyperParam.POWER, [1, 2], x_input_features,
-#                y_output_data, max_iter=5000)
-#
-# cross_validate(SVC, HyperParam.GAMMA, [0.001, 0.01, 0.1, 1, 10, 50, 80], x_input_features,
-#                y_output_data, max_iter=900)
-#
-# cross_validate(SVC, HyperParam.C, [0.001, 0.01, 0.1, 1, 10, 50, 80], x_input_features,
-#                y_output_data, max_iter=900)
 
 # print(f"Ideal num features = {ideal_num_features}")
 
-pyplot.errorbar([0.001, 0.01, 0.1, 1, 5, 10, 15, 20], log_loss_list, yerr=standard_dev, label="Standard Deviation")
-pyplot.title(f"Prediction error vs C - logistic")
-pyplot.xlabel(f"C Value")
-pyplot.ylabel("log loss")
-pyplot.legend()
+# cross_validate(LogisticRegression, HyperParam.C, [0.001, 0.01, 0.1, 1, 5, 10, 15, 20], x_input_features, y_output_data)
+# cross_validate(SVC, HyperParam.GAMMA, [0.000001, 0.00001, 0.0001, 0.001, 0.005, 0.007], x_input_features, y_output_data)
+# cross_validate(SVC, HyperParam.C, [0.001, 0.01, 0.1, 1, 5, 10, 15, 20], x_input_features, y_output_data)
+# cross_validate(KNeighborsClassifier, HyperParam.K, [1, 5, 10, 15, 25, 50, 75, 100, 125, 150, 175, 200],
+#                x_input_features, y_output_data)
+# cross_validate(KNeighborsClassifier, HyperParam.K, [1, 5, 10, 15, 25, 50, 75, 100, 125, 150, 175, 200],
+#                x_input_features, y_output_data, weights="distance")
+# cross_validate(LogisticRegression, HyperParam.POWER, [1, 2], x_input_features, y_output_data, max_iter=1500)
+
+
+logistic_model = LogisticRegression(class_weight='auto', max_iter=900, C=1)  # best from above plots
+svc_model = SVC(gamma=0.001, C=1)
+knn_model = KNeighborsClassifier(n_neighbors=100)
+
+logistic_pipeline = make_pipeline(StandardScaler(), logistic_model)
+logistic_pipeline.fit(x_input_features, np.array(y_output_data).ravel())
+y_pred = logistic_pipeline.predict(test_x_input_features)
+print(f'Logistic Accuracy : {accuracy_score(y_true=test_y_output_data, y_pred=y_pred)}')
+
+svc_pipeline = make_pipeline(StandardScaler(), svc_model)
+svc_pipeline.fit(x_input_features, np.array(y_output_data).ravel())
+y_pred = svc_pipeline.predict(test_x_input_features)
+print(f'SVC Accuracy : {accuracy_score(y_true=test_y_output_data, y_pred=y_pred)}')
+
+knn_pipeline = make_pipeline(StandardScaler(), knn_model)
+knn_pipeline.fit(x_input_features, np.array(y_output_data).ravel())
+y_pred = knn_pipeline.predict(test_x_input_features)
+print(f'kNN Accuracy : {accuracy_score(y_true=test_y_output_data, y_pred=y_pred)}')
+
+best_pipeline = logistic_pipeline
+plot_confusion_matrix(best_pipeline, test_x_input_features, test_y_output_data)
+pyplot.title("Logistic Regression")
 pyplot.show()
 
-logistic_model = LogisticRegression(class_weight='auto', max_iter=900, C=1)  # from above plot
-
-pipeline = make_pipeline(StandardScaler(), logistic_model)
-pipeline.fit(x_training_data, np.array(y_training_data).ravel())
-
-y_pred = pipeline.predict(test_x_input_features)
-print(f'Model Accuracy : {accuracy_score(y_true=test_y_output_data, y_pred=y_pred)}')
-
-# baseline = DummyClassifier(strategy="uniform")
-# baseline = DummyClassifier(strategy="most_frequent")
-baseline = DummyClassifier(strategy="stratified")
+baseline = DummyClassifier(strategy="most_frequent")
 baseline.fit(x_input_features, y_output_data)
-print(f"Baseline Accuracy = {accuracy_score(y_pred=baseline.predict(x_input_features), y_true=y_output_data)}")
+plot_confusion_matrix(baseline, test_x_input_features, test_y_output_data)
+pyplot.title("Stratified Baseline")
+pyplot.show()
+
+print(f"Baseline Accuracy: {accuracy_score(y_pred=baseline.predict(test_x_input_features), y_true=test_y_output_data)}")
